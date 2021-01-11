dbf99ec496b1873b3ec6a3b24a89d275b1176fb6
QRK1TT13 <11841974+QRK1TT13@users.noreply.github.com>
2021-01-11 14:22:31 -0700

    Update bot.py
    Added running file capabilities to allow program to be interrupted during the run and an easy recovery
    Bug fixes

--- bot.py
+++ bot.py
@@ -3,24 +3,54 @@ import requests
 import time
 import datetime
 import sys
+import os
 
 # Globals
-JsonFileLocation = "./config.json"
+JsonFileLocation = "./myconfig.json"
 JsonData = ''
-GroupMessages = []
-DirectMessages = []
+RunningData = {}
 Token = ''
 TokenLife = (0, 0)
 IdTable = ''
 Map = {}
 IMap = {}
-SecondaryMessages = [0, 0, 0]
 
 
 def read_json(file):
 	with open(file) as f:
 		data = json.load(f)
 	return data
+
+
+def make_running_file():
+	print("Log: Writing new Running file")
+	RunningData["day"] = datetime.datetime.today().strftime('%j')
+	RunningData["likes"] = []
+	RunningData["secondary-messages"] = [0, 0, 0]
+	RunningData["group-messages"] = []
+	RunningData["direct-messages"] = []
+	with open(JsonData["running-data"], "w") as write_file:
+		json.dump(RunningData, write_file, indent=4)
+
+
+def ready_running_file():
+	if os.path.exists(JsonData["running-data"]):
+		global RunningData
+		RunningData = read_json(JsonData["running-data"])
+		if datetime.datetime.today().strftime('%j') > RunningData["day"] and datetime.datetime.now() > datetime.datetime.now().replace(hour=1, minute=30):
+			print("Log: Running file is out of date. Removing")
+			os.remove(JsonData["running-data"])
+			make_running_file()
+		else:
+			print("Log: Resuming bot session from today. Day: {}. Likes: {}. Group messages: {}".format(RunningData["day"], len(RunningData["likes"]), RunningData["group-messages"]))
+	else:
+		make_running_file()
+
+
+def save_running_data():
+	os.remove(JsonData["running-data"])
+	with open(JsonData["running-data"], "w") as write_file:
+		json.dump(RunningData, write_file, indent=4)
 
 
 def wait_to_start(hours=19, minutes=15, seconds=0):
@@ -50,43 +80,46 @@ def get_group_info():
 
 
 def send_group_message(message):
-	if len(GroupMessages) > 0 and SecondaryMessages[0] == 0:
-		print("Not sending a new group message.  Reusing {}".format(GroupMessages))
-		SecondaryMessages[0] = 1
-	elif SecondaryMessages[0] == 0 or SecondaryMessages[1] == 0:
+	if len(RunningData["group-messages"]) > 0 and RunningData["secondary-messages"][0] == 0:
+		print("Log: Not sending a new group message.  Reusing {}".format(RunningData["group-messages"]))
+		RunningData["secondary-messages"][0] = 1
+	elif len(RunningData["group-messages"]) > 1 and RunningData["secondary-messages"][1] == 0:
+		print("Log: Not sending a new second group message.  Reusing {}".format(RunningData["group-messages"]))
+		RunningData["secondary-messages"][1] = 1
+	elif RunningData["secondary-messages"][0] == 0 or RunningData["secondary-messages"][1] == 0:
 		groupId = JsonData["group-id"]
 		groupmeAPIKey = JsonData['groupme-api-key']
 		url = "https://api.groupme.com/v3/groups/{}/messages?token={}".format(groupId, groupmeAPIKey)
-		payload = '{"message": {"source_guid": "GUID", "text": ' + message + '}}'
+		payload = '{"message": {"source_guid": "GUID", "text": "' + message + '"}}'
 		headers = {'Content-Type': 'application/json'}
 		if JsonData["simulate"] == 0:
 			response = requests.request("POST", url, headers=headers, data=payload)
-			if response.status_code != 200:
-				print(
-					"Fatal: Groupme returned an error when trying to send a message.  Make sure you have permission for that group or your api key is right.")
+			if response.status_code != 201:
+				print("Fatal: Groupme returned an error when trying to send a message.  Make sure you have permission for that group or your api key is right.")
 				print(response.status_code)
 				print(response.text)
 				exit(1)
-			print("Group message '{}' sent".format(message))
+			print("Log: Group message '{}' sent".format(message))
 			responseJson = json.loads(response.text)
-			messageId = responseJson["response"]["direct_message"]["id"]
-			print("Message id is: {}".format(messageId))
-			GroupMessages.append(messageId)
+			messageId = responseJson["response"]["message"]["id"]
+			print("Log: Message id is: {}".format(messageId))
+			RunningData["group-messages"].append(messageId)
 		else:
-			print("Simulating group message, no message sent")
+			print("Log: Simulating group message, no message sent")
 		# This system assumes the best
-		if SecondaryMessages[0] == 0:
-			SecondaryMessages[0] = 1
-		elif SecondaryMessages[1] == 0:
-			SecondaryMessages[1] = 1
+		if RunningData["secondary-messages"][0] == 0:
+			RunningData["secondary-messages"][0] = 1
+		elif RunningData["secondary-messages"][1] == 0:
+			RunningData["secondary-messages"][1] = 1
 	else:
-		print("Fatal: send_group_message has been called 3 times.  How did this even happen?")
+		print("Fatal: send_group_message has been called 3 times?  How did this even happen?")
+		exit(1)
 
 
 def send_direct_messages(id, message):
 	groupmeAPIKey = JsonData['groupme-api-key']
 	url = "https://api.groupme.com/v3/direct_messages?token={}".format(groupmeAPIKey)
-	payload = '{"direct_message": {"source_guid": "GUID","recipient_id": "' + id + '", "text\": "' + message + '"}}'
+	payload = '{"direct_message": {"source_guid": "'+id+'","recipient_id": "' + id + '", "text\": "' + message + '"}}'
 	headers = {'Content-Type': 'application/json'}
 	if JsonData["simulate"] == 0:
 		response = requests.request("POST", url, headers=headers, data=payload)
@@ -95,8 +128,9 @@ def send_direct_messages(id, message):
 		else:
 			responseJson = json.loads(response.text)
 			messageId = responseJson["response"]["direct_message"]["id"]
-			DirectMessages.append([id, messageId])
-	SecondaryMessages[2] = 1
+			RunningData["direct-messages"].append([id, messageId])
+			print("Log: Direct message sent to {}, id {} sent.  Message id: {}".format(Map[id]["name"], id, [id, messageId]))
+	RunningData["secondary-messages"][2] = 1
 
 
 def check_message_likes(type, id, message):
@@ -105,7 +139,7 @@ def check_message_likes(type, id, message):
 	if type == "group":
 		url = "https://api.groupme.com/v3/groups/{}/messages/{}?token={}".format(id, message, groupmeAPIKey)
 	elif type == "direct":
-		url = "https://api.groupme.com/v3/direct_messages?token={}&other_user_id={}&since_id={}&before_id={}".format(groupmeAPIKey, id, message - 1, message + 1)
+		url = "https://api.groupme.com/v3/direct_messages?token={}&other_user_id={}&since_id={}&before_id={}".format(groupmeAPIKey, id, str(int(message) - 1), str(int(message) + 1))
 	headers = {'Content-Type': 'application/json'}
 	response = requests.request("GET", url, headers=headers)
 	if response.status_code != 200:
@@ -123,9 +157,9 @@ def check_message_likes(type, id, message):
 def get_messages_likes():
 	likes = []
 	groupId = JsonData["group-id"]
-	for message in GroupMessages:
+	for message in RunningData["group-messages"]:
 		likes.extend(check_message_likes("group", groupId, message))
-	for user, message in DirectMessages:
+	for user, message in RunningData["direct-messages"]:
 		likes.extend(check_message_likes("direct", user, message))
 	likes = list(set(likes))
 	likes.sort()
@@ -187,9 +221,14 @@ def get_token():
 		response = requests.request("POST", url, headers=headers)
 		if response.status_code != 200:
 			print("Warning: Cannot update sharepoint token. Code: {}, message: {}".format(response.status_code, response.text))
-		jsonResponse = json.loads(response.text)
-		Token = jsonResponse["d"]["GetContextWebInformation"]["FormDigestValue"]
-		TokenLife = (int(time.time()), jsonResponse["d"]["GetContextWebInformation"]["FormDigestTimeoutSeconds"])
+			time.sleep(1)
+			print("Warning: Retrying token update")
+			get_token()
+			return
+		else:
+			jsonResponse = json.loads(response.text)
+			Token = jsonResponse["d"]["GetContextWebInformation"]["FormDigestValue"]
+			TokenLife = (int(time.time()), jsonResponse["d"]["GetContextWebInformation"]["FormDigestTimeoutSeconds"])
 
 
 def update_DI_times(ids):
@@ -229,9 +268,10 @@ def sign_di_roster(ids):
 	for id in ids:
 		# Updating Token
 		get_token()
-		userInfo = json.loads(get_user_info(id))
+		userInfo = get_user_info(id)
 		if userInfo == -1:
 			break
+		userInfo = json.loads(userInfo)
 		headers = {'Cookie': JsonData["sharepoint-cookie"], 'Accept': 'application/json;odata=verbose', 'X-RequestDigest': Token, 'Content-Type': 'application/json'}
 		payload = '{"'+JsonData["sharepoint-di-email"]+'": "'+userInfo["d"][JsonData["sharepoint-roster-email"]]+'", "'+JsonData["sharepoint-di-unit"]+'": "'+userInfo["d"][JsonData["sharepoint-roster-unit"]]+'", "'+JsonData["sharepoint-di-signat"]+'": "'+datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")+'", "'+JsonData["sharepoint-di-signby"]+'": "'+JsonData["bot-name"]+'", "'+JsonData["sharepoint-di-name"]+'": "'+userInfo["d"][JsonData["sharepoint-roster-name"]]+'"}'
 		if JsonData["simulate"] == 0:
@@ -256,14 +296,16 @@ if __name__ == "__main__":
 	# Wait until 7:15 to start body of script
 	wait_to_start()
 	print("Log: Starting bot at {}".format(datetime.datetime.now()))
-	# Keep track of likes
-	oldLikes = []
 	# Read config file
 	JsonData = read_json(JsonFileLocation)
+	# Load running file
+	ready_running_file()
+	RunningData = read_json(JsonData["running-data"])
 	# Get group info
 	groupInfo = get_group_info()
 	# Send message to group
-	send_group_message(JsonData["group-message-text"])
+	if RunningData["secondary-messages"][0] == 0:
+		send_group_message(JsonData["group-message-text"])
 	# Convert groupme ids to sharepoint ids
 	IdTable = load_id_table()
 	# Build ID maps for reference
@@ -271,9 +313,9 @@ if __name__ == "__main__":
 	build_maps(groupInfo)
 	print("..Done")
 	# Set times to send reminder messages and when to end the program
-	secondMessageTime = datetime.datetime.today().replace(hour=20, minute=0, second=0)
-	dmTime = datetime.datetime.today().replace(hour=20, minute=30, second=0)
-	endTime = datetime.datetime.today().replace(hour=1, minute=30, second=0) + datetime.timedelta(days=1)
+	secondMessageTime = datetime.datetime.today().replace(hour=20, minute=0, second=0, microsecond=0)
+	dmTime = datetime.datetime.today().replace(hour=20, minute=30, second=0, microsecond=0)
+	endTime = datetime.datetime.today().replace(hour=1, minute=30, second=0, microsecond=0) + datetime.timedelta(days=1)
 	current = datetime.datetime.now()
 	# Start loop to run checks for likes.  Will send new messages based on annoying parameter
 	while current < endTime:
@@ -282,7 +324,7 @@ if __name__ == "__main__":
 		# Check if any message has likes
 		foundLikes = get_messages_likes()
 		# Compare against seen likes to get new likes
-		newLikes = get_new_likes(oldLikes, foundLikes)
+		newLikes = get_new_likes(RunningData["likes"], foundLikes)
 		# Convert liked ids to sharepoint ids
 		newIds = convert_ids(newLikes)
 		# Update DI roster time
@@ -293,22 +335,23 @@ if __name__ == "__main__":
 		completed = intersection_array(completedRoster, completedTimes)
 		for id in completed:
 			print("Log: Bot has signed for {}".format(IMap[id]["name"]))
-		oldLikes.extend(newLikes)
+		RunningData["likes"].extend(newLikes)
 		# If 9 pm send another message
-		if current >= secondMessageTime and SecondaryMessages[1] == 0 and 2 <= JsonData["annoying-level"]:
+		if current >= secondMessageTime and RunningData["secondary-messages"][1] == 0 and 2 <= JsonData["annoying-level"]:
 			print("Log: Sending group text 2")
 			send_group_message(JsonData["group-message-text2"])
 		# If 10 pm send individual messages
-		if current >= dmTime and SecondaryMessages[2] == 0 and 3 == JsonData["annoying-level"]:
+		if current >= dmTime and RunningData["secondary-messages"][2] == 0 and 3 == JsonData["annoying-level"]:
 			print("Log: Sending dms")
 			missing = []
 			for id in IdTable:
-				if id not in oldLikes and IdTable[id] != 'null':
+				if id not in RunningData["likes"] and IdTable[id] != 'null':
 					missing.append(id)
 			if len(missing) == 0:
 				break
 			for id in missing:
 				send_direct_messages(id, JsonData["dm-message-text"])
+		save_running_data()
 		time.sleep(60 - time.time() % 60)
 		current = datetime.datetime.now()
 	print("Log: LikeForDI should be all done. {}".format(datetime.datetime.now()))
